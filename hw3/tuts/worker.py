9fbbf66eeac736d4dd34c7ac069e30f8f376156d
Johnny So <jso123450@gmail.com>
2019-03-06 04:43:54 -0500

    Testing rabbitmq tutorials

--- hw3/tuts/worker.py
+++ hw3/tuts/worker.py
@@ -4,17 +4,17 @@ import time
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     time.sleep(body.count(b'.'))
     print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
+channel.basic_qos(prefetch_count=1)
 channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+                      queue='task_queue')
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
