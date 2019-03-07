629c3e51e40f420ccfcdf0f41fadfa73e8291a84
Johnny So <jso123450@gmail.com>
2019-03-07 05:02:32 -0500

    Update main.py

--- hw3/main.py
+++ hw3/main.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify, request
 from consumer import listen_on
 from producer import publish_on
-from constants import JSON_KEY_STATUS, JSON_KEY_KEY, JSON_KEY_KEYS, JSON_KEY_MSG, JSON_VAL_ERR, \
-						JSON_VAL_OK, STATUS_ERR, STATUS_OK
+# from constants import JSON_KEY_STATUS, JSON_KEY_KEY, JSON_KEY_KEYS, JSON_KEY_MSG, JSON_VAL_ERR, \
+# 						JSON_VAL_OK, STATUS_ERR, STATUS_OK
 
-import threading
+JSON_KEY_STATUS = 'status'
+JSON_KEY_KEY = 'key'
+JSON_KEY_KEYS = 'keys'
+JSON_KEY_MSG = 'msg'
+JSON_VAL_ERR = 'ERROR'
+JSON_VAL_OK = 'OK'
+
+STATUS_ERR = {}
+STATUS_ERR[JSON_KEY_STATUS] = JSON_VAL_ERR
+
+STATUS_OK = {}
+STATUS_OK[JSON_KEY_STATUS] = JSON_VAL_OK
 
 app = Flask(__name__)
 
