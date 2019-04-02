6f14e5c4ea011305c12b05f30188329fe28df025
Johnny So <jso123450@gmail.com>
2019-04-01 20:11:13 -0400

    Testing hw5

---
+++ hw5/constants.js
@@ -0,0 +1,15 @@
+module.exports = {
+    LOCALHOST: '127.0.0.1',
+    CASSANDRA_PORT: '9042',
+    CASSANDRA_DATACENTER: 'datacenter1',
+    CASSANDRA_KEYSPACE: 'hw5',
+    CASSANDRA_TABLE: 'imgs',
+    EXPRESS_PORT: '8000',
+
+    KEY_FILENAME: 'filename',
+    KEY_CONTENTS: 'contents',
+    KEY_MIMETYPE: 'mime',
+
+    ERR_MISSING_PARAMS: 'Required parameters are missing from the request.',
+    ERR_FILE_DOESNT_EXIST: 'Specified file does not exist.'
+};
---
+++ hw5/index.js
@@ -0,0 +1,89 @@
+/* library imports */
+const express = require('express');
+const multer = require('multer');
+const cassandra = require('cassandra-driver');
+
+/* project imports */
+const constants = require('./constants');
+
+/* initialize libraries */
+var upload = multer();
+var client = new cassandra.Client({ contactPoints: [constants.LOCALHOST + ':' + constants.CASSANDRA_PORT], 
+                                    localDataCenter: constants.CASSANDRA_DATACENTER, 
+                                    keyspace: constants.CASSANDRA_KEYSPACE });
+const INSERT_STATEMENT = `INSERT INTO ${constants.CASSANDRA_TABLE} (${constants.KEY_FILENAME}, ${constants.KEY_CONTENTS}, ${constants.KEY_MIMETYPE}) VALUES (?, ?, ?)`;
+const SELECT_STATEMENT = `SELECT ${constants.KEY_CONTENTS}, ${constants.KEY_MIMETYPE} FROM ${constants.CASSANDRA_TABLE} WHERE ${constants.KEY_FILENAME} = ?`;
+
+/* initialize express application */
+const app = express();
+app.use(express.json());
+
+const PORT = constants.EXPRESS_PORT;
+
+/* helper funcs */
+function generateOK(){
+    let response = {};
+    response[constants.STATUS_KEY] = constants.STATUS_OK;
+    return response;
+}
+
+function generateERR(){
+    let response = {};
+    response[constants.STATUS_KEY] = constants.STATUS_ERR;
+    response[constants.STATUS_ERR] = '';
+    return response;
+}
+
+/* endpoints */
+app.post('/deposit', upload.single(constants.KEY_CONTENTS), async(req, res) => {
+    // the file specified as constants.KEY_CONTENTS in the form is in req.file
+    let response = generateERR();
+
+    let filename = req.body.filename;
+    let file = req.file;
+    let mime = req.file.mimetype;
+
+    if (filename == undefined || file == undefined || mime == undefined){
+        response[constants.STATUS_ERR] = constants.ERR_MISSING_PARAMS;
+        return res.json(response);
+    }
+
+    const params = [filename, file, mime];
+    client.execute(INSERT_STATEMENT, params, { prepare: true})
+        .then(result => console.log(`Uploaded ${filename} with mimetype ${mime}`));
+
+    response = generateOK();
+    return res.json(response);
+});
+
+app.get('/retrieve', async(req, res) => {
+    let response = generateERR();
+
+    let filename = req.body.filename;
+
+    if (filename == undefined){
+        response[constants.STATUS_ERR] = constants.ERR_MISSING_PARAMS;
+        return res.json(response);
+    }
+
+    const params = [filename];
+
+    let file = undefined;
+    let mime = undefined;
+    client.execute(SELECT_STATEMENT, params)
+        .then((result) => { 
+                file = result.rows[0].contents;
+                mime = result.rows[0].mime;
+            });
+    
+    if (file == undefined || mime == undefined){
+        response[constants.STATUS_ERR] = constants.ERR_FILE_DOESNT_EXIST;
+        return res.json(response);
+    }
+
+    res.contentType(mime);
+    res.send(file);
+});
+
+/* Start the server. */
+app.listen(PORT, () => console.log(`Server running on http://127.0.0.1:${PORT}`));
